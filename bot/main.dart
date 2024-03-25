aa701255393a5b1a1fbb4cb24dc741e42d34a732
shceo <fayzullojalolov42@gmail.com>
2024-03-25 23:01:44 +0500

    big update

--- bot/main.dart
+++ bot/main.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:dart_telegram_bot/dart_telegram_bot.dart';
 import 'package:dart_telegram_bot/telegram_entities.dart';
 import 'package:http/http.dart' as http;
+import 'package:dart_telegram_bot/dart_telegram_bot.dart';
 
 class MyBot extends Bot {
-  MyBot(String token) : super(token: token);
+  final String openExchangeRatesToken;
+  final String openWeatherMapToken;
+
+  MyBot(String token, this.openExchangeRatesToken, this.openWeatherMapToken)
+      : super(token: token);
 
   Future<Future<Message>> onBotCommand(Bot bot, Update update) async {
     var args = BotCommandParser.fromMessage(update.message!)!.args;
@@ -25,24 +28,25 @@ class MyBot extends Bot {
             'Invalid number of arguments. Usage: курс {валюта}',
           );
         }
-        var currency = args[1];
+        var currency = args[1].toUpperCase();
         var rate = await getCurrencyRate(currency);
         return bot.sendMessage(
           ChatID(update.message!.chat.id),
           'Курс $currency: $rate',
         );
       case 'погода':
-        if (args.length != 2) {
+        if (args.length != 3) {
           return bot.sendMessage(
             ChatID(update.message!.chat.id),
-            'Invalid number of arguments. Usage: погода {страна/город}',
+            'Invalid number of arguments. Usage: погода {страна} {город}',
           );
         }
-        var location = args[1];
-        var weather = await getWeather(location);
+        var country = args[1];
+        var city = args[2];
+        var weather = await getWeather(country, city);
         return bot.sendMessage(
           ChatID(update.message!.chat.id),
-          'Погода в $location: $weather',
+          'Погода в $city, $country: $weather',
         );
       default:
         return bot.sendMessage(
@@ -51,79 +55,41 @@ class MyBot extends Bot {
         );
     }
   }
-
-@override
-Future<void> onReady(Bot bot) async {
-  print('Bot ${bot.name} ready');
-  onCommand('курс', onBotCommand);
-  onCommand('погода', onBotCommand);
-  await start(clean: true);
-}
-
 
   @override
-  Future<void> onUpdate(Future<void> Function(Bot, Update) handler) async {
-    super.onUpdate(handler);
-  }
-
-  Future<Future<Message>> onBotCallbackQuery(Bot bot, CallbackQuery callbackQuery) async {
-    var args = callbackQuery.data?.split(' ');
-    var chatId = callbackQuery.message!.chat.id;
-
-    switch (args?[0]) {
-      case 'курс':
-        if (args?.length != 2) {
-          return bot.sendMessage(
-            ChatID(chatId),
-            'Invalid number of arguments. Usage: курс {валюта}',
-          );
-        }
-        var currency = args?[1];
-        var rate = await getCurrencyRate(currency!);
-        return bot.sendMessage(
-          ChatID(chatId),
-          'Курс $currency: $rate',
-        );
-      case 'погода':
-        if (args?.length != 2) {
-          return bot.sendMessage(
-            ChatID(chatId),
-            'Invalid number of arguments. Usage: погода {страна/город}',
-          );
-        }
-        var location = args?[1];
-        var weather = await getWeather(location!);
-        return bot.sendMessage(
-          ChatID(chatId),
-          'Погода в $location: $weather',
-        );
-      default:
-        return bot.sendMessage(
-          ChatID(chatId),
-          'Unknown command',
-        );
-    }
+  Future<void> onReady(Bot bot) async {
+    print('Bot ${bot.name} ready');
+    onCommand('курс', onBotCommand);
+    onCommand('погода', onBotCommand);
+    await start(clean: true);
   }
 
   Future<String> getCurrencyRate(String currency) async {
-    // Ваш код для получения курса валюты
-    return '10'; // Пример
+    var url = Uri.parse(
+        'https://openexchangerates.org/api/latest.json?app_id=$openExchangeRatesToken&base=USD');
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+    var rates = data['rates'];
+    var rate = rates[currency];
+    return rate.toString();
   }
 
-  Future<String> getWeather(String location) async {
-    // Ваш код для получения погоды
-    return 'Солнечно'; // Пример
+  Future<String> getWeather(String country, String city) async {
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city,$country&appid=$openWeatherMapToken');
+    var response = await http.get(url);
+    var data = jsonDecode(response.body);
+    var weather = data['weather'][0]['description'];
+    return weather;
   }
 }
 
 void main() async {
-  var token = Platform.environment['7013856621:AAG-mO45dfP_iZAcz7pytvWfZR0dx31oyQQ'];
+  var token = Platform.environment['токен для бота телеграм'];
+  var openExchangeRatesToken =
+      Platform.environment['токен для валюты'];
+  var openWeatherMapToken = Platform.environment['токен для погоды'];
 
-  if (token == null) {
-    print('Ошибка: Токен бота не был найден в переменных окружения.');
-    return;
-  }
-
-  var bot = MyBot(token);
-  await bot.onReady(bot); // Передаем экземпляр класса bot в метод onReady
+  MyBot(token ?? '', openExchangeRatesToken ?? '', openWeatherMapToken ?? '')
+      .onReady(Bot(token: '$token'));
 }
